e7f43bd0e3b82eda9cb7370cb2f3dc605789eaaa
Robkata <r.georgiev2017@pmg-pernik.com>
2021-09-29 13:43:44 +0300

    Edit

---
+++ Array/Print Numbers in Reverse Order/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Print_Numbers_in_Reverse_Order
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+
+            IEnumerable<int> reversedList = array.Reverse();
+
+            foreach (int arr in reversedList)
+            {
+                Console.WriteLine(String.Join(", ", arr));
+            }
+        }
+    }
+}
---
+++ Array/Reverse Array of Strings/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Reverse_Array_of_Strings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] words = Console.ReadLine().Split(' ').ToArray();
+
+            IEnumerable<string> reversedList = words.Reverse();
+
+            foreach (string name in reversedList)
+            {
+                Console.WriteLine(String.Join(", ", name));
+            }
+        }
+    }
+}
